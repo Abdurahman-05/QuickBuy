5cbf241ffea457cd034b36b92389786b64c9c7cc
Abdurahman <eabdurahman8@gmail.com>
2026-04-07 08:55:59 +0300

    Initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+// import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+
+export default function App() {
+  return (
+    <div className="app">
+       <section>
+      <h1>QuickBuy</h1>
+    
+    </section>
+    </div>
+  );
+}
