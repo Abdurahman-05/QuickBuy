b533f8743be3d79ecd58c66c3952a98cfb7a4633
Abdurahman <eabdurahman8@gmail.com>
2026-04-16 11:17:50 +0300

    modify folder structure and route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/ui/Footer";
-import Navbar from "./components/ui/Navbar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
-import Register from "./pages/Register"; // 1. Import your new page
+import Register from "./pages/Register";
+import UserProfilePage from "./pages/UserProfilePage";
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
   return (
-    <div className="app min-h-screen flex flex-col bg-[#f5f5f5]">
-      <Navbar />
-      <div className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productDetail" element={<ProductDetails />} />
-          <Route path="/register" element={<Register />} /> {/* 2. Add the route */}
-        </Routes>
-      </div>
-      <Footer />
-    </div>
+    <Routes>
+      {/* Pages WITH navbar + footer */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/productDetail" element={<ProductDetails />} />
+      </Route>
+
+
+      <Route path="/profile" element={<UserProfilePage />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
